7e91f6eddc2f94e28baa9c2fc4ff37f55c724199
usamahn <oussama.henia@outlook.fr>
2021-07-11 22:19:19 +0100

    Init

---
+++ front/src/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import theme from "assets/theme/theme.js";
+
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-dashboard-react.scss";
+
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
+
+import AdminLayout from "layouts/Admin.js";
+import AuthLayout from "layouts/Auth.js";
+
+
+import Index from "views/Index.js";
+import Landing from "views/examples/Landing.js";
+import Login from "views/examples/Login.js";
+import Profile from "views/examples/Profile.js";
+import Register from "views/examples/Register.js";
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+        <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+        <Route
+          path="/landing-page"
+          exact
+          render={props => <Landing {...props} />}
+        />
+        <Route path="/login-page" exact render={props => <Login {...props} />} />
+        <Route
+          path="/profile-page"
+          exact
+          render={props => <Profile {...props} />}
+        />
+        <Route
+          path="/register-page"
+          exact
+          render={props => <Register {...props} />}
+        />
+        <Redirect from="/" to="/admin/index" />
+      </Switch>
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.querySelector("#root")
+);
